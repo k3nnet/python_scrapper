335e53ae2eb69a30e535d9378ec4c34d623d519c
k3nnet <kkmphele@gmail.com>
2019-09-08 09:17:32 +0200

    refractored to create a stock ticker scrapper that collects information about a stock and return a json format file . the file is used to create  a rest api

--- scraper.py
+++ scraper.py
@@ -4,33 +4,44 @@ import json
 
 #request site page by passing in url and returning a html document
 def getPage():
-    URL="https://classybrain.com/google-my-business-categories-2018/"
+    URL="https://www.african-markets.com/en/stock-markets/jse/listed-companies"
     raw_page_data=requests.get(URL)
     raw_page_data.raise_for_status()
     return raw_page_data;
    
 
 def toJson(data):
-    with open('data.json', 'w') as outfile:
+   # print(data)
+    with open('stocks.json', 'w') as outfile:
         json.dump(data, outfile)
 
 #scrape the site for property details
 def scrapeSite(site):
     content=[]
-    property={}
-    categories=[]
+    
+    stocks=[]
     soup = bs4.BeautifulSoup(site.text,'html.parser')
     content=soup.find_all('tbody')
-    #print(content)
+    print()
  
-    for link in content:
-        
-        for linke in link.find_all('td'):
-                categories.append(linke.get_text())
-                print(categories)
-       
+    for link in content[0].find_all('tr'):
+        stock={}
+        print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+        print(link.select('.ari-tbl-col-0')[0])
+        stock['name']=link.select('.ari-tbl-col-0')[0].get_text()
+        stock['symbol']=link.select('.ari-tbl-col-1')[0].get_text()
+        stock['category']=link.select('.ari-tbl-col-2')[0].get_text()
+        print(stock)
+        stocks.append(stock)  
+               
+                
     
-    return categories;
+        
+              
+               
+       
+    print(stocks)
+    return stocks;
 
 if __name__ == "__main__":
  data= scrapeSite(getPage())
